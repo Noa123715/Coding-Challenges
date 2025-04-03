6e4c5ec47533bddaa8493971e9e82c01cfc56f5e
noa123715 <noa123715@gmail.com>
2025-04-03 12:00:19 +0300

    Finish Part A

--- Part A/Section B/time_series_processing.py
+++ Part A/Section B/time_series_processing.py
@@ -1,3 +1,4 @@
+import shutil
 import constants
 import os
 import pandas as pd
@@ -81,6 +82,7 @@ def calculate_hourly_average(file_path):
         answer_avg = valid_rows.groupby('hour')['value'].mean().reset_index()
         
         return answer_avg
+
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
@@ -108,7 +110,7 @@ def split_file_by_day(file_path, split_folder, type='csv'):
             if type == 'parquet':
                 group.to_parquet(f"{split_folder}/time_series_{date.date()}.parquet", index=False)
         
-        return "File successfully split by date."
+        print("File successfully split by date.")
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
@@ -117,14 +119,14 @@ def process_and_combine_avg(split_folder, output_file):
     try:
         all_avg = []  # List to store all the hourly averages
         
-        # Get a list of all split files
-        split_files = [idx for idx in os.listdir(split_folder) if idx.endswith('.csv')]
+        # Get a list of all csv split files
+        split_files = [idx for idx in os.listdir(split_folder) if idx.endswith('.csv') or idx.endswith('.parquet')]
         
         # Process each split file
         for s_file in split_files:
             sf_path = os.path.join(split_folder, s_file)  # split file path
             # Calculate hourly averages for the current split file
-            hourly_avg = calculate_hourly_average(sf_path)  # Using your existing function
+            hourly_avg = calculate_hourly_average(sf_path)
             
             # Append the results to the list
             all_avg.append(hourly_avg)
@@ -136,42 +138,44 @@ def process_and_combine_avg(split_folder, output_file):
         combined_avg = combined_avg.sort_values(by='hour')
         
         # Save the final combined result to a CSV file
-        combined_avg.to_csv(output_file, index=False)
+        combined_avg.to_parquet(output_file, index=False)
         
-        return f"Hourly averages combined and saved to {output_file}."
+        print(f"Hourly averages combined and saved to {output_file}.")
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-def main():
-    # split the CSV file
-    split_status_csv = split_file_by_day(constants.INPUT_CSV_FILE, constants.SPLIT_FOLDER, type='csv')
-    print(split_status_csv)
-    
-    # split the Parquet file
-    split_status_parquet = split_file_by_day(constants.INPUT_PARQUET_FILE, constants.SPLIT_FOLDER, type='parquet')
-    print(split_status_parquet)
-    
-    # calculate the hourly average for each split file
-    status_csv = process_and_combine_avg(constants.SPLIT_FOLDER, constants.OUTPUT_CSV_FILE)
-    print(status_csv)
-    
-    # Combine status for Parquet file
-    status_parquet = process_and_combine_avg(constants.SPLIT_FOLDER, constants.OUTPUT_PARQUET_FILE)
-    print(status_parquet)
-    
-    # Combine both CSV and Parquet files
-    csv_parquet = pd.concat([pd.read_csv(constants.OUTPUT_CSV_FILE), pd.read_csv(constants.OUTPUT_PARQUET_FILE)], ignore_index=True)
-    
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as temp_file:
-        temp_file_path = temp_file.name
-        csv_parquet.to_csv(temp_file_path, index=False)
+    finally:
+        # remove the tmp folder
+        if os.path.exists(split_folder):
+            shutil.rmtree(split_folder)
+            print(f"The {split_folder} folder has been removed.")
 
-    # calculate the final average
-    final_avg = calculate_hourly_average(temp_file_path)
+def main():
+
+    is_csv_valid = validate_file(read_file(constants.INPUT_CSV_FILE))
+    is_parquet_valid = validate_file(read_file(constants.INPUT_PARQUET_FILE))
+
+    if is_csv_valid == "Valid" and is_parquet_valid == "Valid":
+        print("Both files are valid and will be processed")
+        # split the 2 files
+        split_file_by_day(constants.INPUT_CSV_FILE, constants.SPLIT_FOLDER, type='csv')
+        split_file_by_day(constants.INPUT_PARQUET_FILE, constants.SPLIT_FOLDER, type='parquet')
     
-    # save the final result
-    final_avg.to_csv(constants.FINAL_OUTPUT_FILE, index=False)
-    print(f"Hourly averages have been saved to {constants.FINAL_OUTPUT_FILE}")
+        # Combine all the tmp file to the final response
+        process_and_combine_avg(constants.SPLIT_FOLDER, constants.OUTPUT_FILE)
+
+    if is_csv_valid == "Valid" and is_parquet_valid != "Valid":
+        print("Only the csv file is valid and will be processed")
+        split_file_by_day(constants.INPUT_CSV_FILE, constants.SPLIT_FOLDER, type='csv')
+        process_and_combine_avg(constants.SPLIT_FOLDER, constants.OUTPUT_FILE)
+
+    if is_parquet_valid == "Valid" and is_csv_valid != "Valid":
+        print("Only the parquet file is valid and will be processed")
+        split_file_by_day(constants.INPUT_PARQUET_FILE, constants.SPLIT_FOLDER, type='parquet')
+        process_and_combine_avg(constants.SPLIT_FOLDER, constants.OUTPUT_FILE)
+
+    if is_csv_valid != "Valid" and is_parquet_valid != "Valid":
+        print("The files is not valid, please try again with valid files")
 
 if __name__ == "__main__":
     main()
